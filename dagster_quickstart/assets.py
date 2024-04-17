e5e777dec631f642fa8c123408b33eb8967d1009
Savarnika <savarnika025@gmail.com>
2024-04-17 21:46:02 +0100

    add naptan data asset

--- dagster_quickstart/assets.py
+++ dagster_quickstart/assets.py
@@ -7,8 +7,9 @@ from dagster import (
     MaterializeResult,
     MetadataValue,
     asset,
+    get_dagster_logger,
 )
-from dagster_quickstart.configurations import HNStoriesConfig
+from dagster_quickstart.configurations import HNStoriesConfig, NaptanConfig
 
 
 @asset
@@ -38,5 +39,49 @@ def hackernews_top_stories(config: HNStoriesConfig) -> MaterializeResult:
         metadata={
             "num_records": len(df),
             "preview": MetadataValue.md(str(df[["title", "by", "url"]].to_markdown())),
+        }
+    )
+
+
+def add_geojson(row):
+    """
+    Return GeoJSON from Latitude and Longitude coordinates for each row of Dataframe
+    """
+    return {
+        "type": "Point",
+        "coordinates": [row['longitude'], row['latitude']]
+    }
+
+
+def pascal_to_snake(s):
+    """
+    Convert PascalCase to snake_case.
+    """
+    # Check each letter in the string; Convert to lower case if the character is
+    # not the first letter, is in upper case and if its adjacent character is in lower case
+    snake_case = ''.join(['_' + c.lower() if c.isupper() and s[i+1].islower() and i!=0 else c.lower() for i, c in enumerate(s)])
+    return snake_case
+
+
+@asset
+def naptan_stops_data(config: NaptanConfig):
+    """Get stops from the Naptan access nodes endpoint and add geom column."""
+
+    # Read data from Naptan API as a Pandas Dataframe
+    naptan_stops_df = pd.read_csv("https://naptan.api.dft.gov.uk/v1/access-nodes?dataFormat=csv")
+    # Publish the DataFrame as a CSV file in a tmp/ folder
+    naptan_stops_df.to_csv(config.naptan_raw_data_path)
+
+    # Convert the column names from Pascal Case to Snake Case to maintain a database standard
+    naptan_stops_df.columns = naptan_stops_df.columns.map(pascal_to_snake)
+    # Add geom column by using longitude and latitude columns
+    naptan_stops_df['geom'] = naptan_stops_df.apply(add_geojson, axis=1)
+    # Publish the cleaned DataFrame to a CSV file in a tmp/folder
+    naptan_stops_df.to_csv(config.naptan_clean_data_path, index=False)
+
+    return MaterializeResult(
+        metadata={
+            "num_records": len(naptan_stops_df),
+            "naptan_data_path": MetadataValue.md(str(config.naptan_clean_data_path)),
         }
     )
--- dagster_quickstart/configurations.py
+++ dagster_quickstart/configurations.py
@@ -5,3 +5,7 @@ class HNStoriesConfig(Config):
     top_stories_limit: int = 10
     hn_top_story_ids_path: str = "hackernews_top_story_ids.json"
     hn_top_stories_path: str = "hackernews_top_stories.csv"
+
+class NaptanConfig(Config):
+    naptan_raw_data_path: str = "tmp/naptan_raw_data.csv"
+    naptan_clean_data_path: str = "tmp/naptan_clean_data.csv"
