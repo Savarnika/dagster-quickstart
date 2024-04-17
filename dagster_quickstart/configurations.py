13cfde3dff4b99147eb5da54e3b491ed458271dc
Savarnika <savarnika025@gmail.com>
2024-04-17 21:49:44 +0100

    Update configurations.py

--- dagster_quickstart/configurations.py
+++ dagster_quickstart/configurations.py
@@ -6,6 +6,7 @@ class HNStoriesConfig(Config):
     hn_top_story_ids_path: str = "hackernews_top_story_ids.json"
     hn_top_stories_path: str = "hackernews_top_stories.csv"
 
+
 class NaptanConfig(Config):
     naptan_raw_data_path: str = "tmp/naptan_raw_data.csv"
     naptan_clean_data_path: str = "tmp/naptan_clean_data.csv"
